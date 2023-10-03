fe020c083c993ca672f342936f909d3248f311fd
ArkharovDanil <maslinaraz@gmail.com>
2023-10-03 14:44:33 +0300

    1234

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloSecondGroup
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Ira");
         }
     }
 }
